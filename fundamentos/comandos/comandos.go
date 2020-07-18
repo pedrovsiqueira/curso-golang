e2de9ea2c9c63314c59adaef8ea7dd296216724d
pedrovsiqueira <pedro.v.siqueira@gmail.com>
2020-07-18 15:51:10 -0300

    feat: start learning about go commands

---
+++ fundamentos/comandos/comandos.go
@@ -0,0 +1,13 @@
+package main
+
+/*
+COMANDOS
+	go vet comandos.go para verificar possíveis erros
+	go build comandos.go para compilar o código
+	go run comandos.go para executar e compilar o código
+*/
+import "fmt"
+
+func main(){
+	fmt.Printf("Outro programa em %s!", "Go")
+}
